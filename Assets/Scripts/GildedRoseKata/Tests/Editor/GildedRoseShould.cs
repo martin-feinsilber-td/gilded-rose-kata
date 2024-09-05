a060377f69c24b8403d48429868fc1109980129c
MiloszLoszek <milosz.loszek@tripledotstudios.com>
2024-09-05 16:11:44 +0200

    first blood

--- Assets/Scripts/GildedRoseKata/Tests/Editor/GildedRoseShould.cs
+++ Assets/Scripts/GildedRoseKata/Tests/Editor/GildedRoseShould.cs
@@ -1,3 +1,7 @@
+using System.Collections.ObjectModel;
+using GildedRoseKata.Context;
+using GildedRoseKata.Domain;
+using GildedRoseKata.Presentation;
 using NUnit.Framework;
 
 namespace GildedRoseKata.Tests.Editor
@@ -9,6 +13,29 @@ namespace GildedRoseKata.Tests.Editor
         public void Rename_Me()
         {
             Assert.Pass();
+        }
+
+        [Test]
+        [TestCase()]
+        public void Item_Should_Have_Quality_Decreased_When_Time_Passes(int qualit)
+        {
+            var item = new Item("", 5, 5);
+            var gildedRose = CreateGildedRoseWithAnItem(item);
+            
+            gildedRose.UpdateQuality();
+
+            Assert.AreEqual(4, item.Quality);
+        }
+        
+        [TestCase()]
+        public void Item_Never_Has_Quality_Less_Than_0()
+        {
+            
+        }
+
+        private GildedRose CreateGildedRoseWithAnItem(Item item)
+        {
+            return new GildedRose(new Collection<Item> { item });
         }
     }
 }
