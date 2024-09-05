c7d5f5c8c0d023feea70616cde641e041b9d3df1
AlbertTDS <albert.dols@tripledotstudios.com>
2024-09-05 16:27:42 +0200

    First part of the solution

--- Assets/Scripts/GildedRoseKata/Tests/Editor/GildedRoseShould.cs
+++ Assets/Scripts/GildedRoseKata/Tests/Editor/GildedRoseShould.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using GildedRoseKata.Domain;
+using GildedRoseKata.Presentation;
 using NUnit.Framework;
 
 namespace GildedRoseKata.Tests.Editor
@@ -5,10 +8,42 @@ namespace GildedRoseKata.Tests.Editor
     [TestFixture]
     public class GildedRoseShould 
     {
-        [Test]
-        public void Rename_Me()
+        [TestCase(2, 4)]
+        public void Not_Have_Negative_Item_Quality(int initQuality, int daysPassed)
         {
-            Assert.Pass();
+            Item testItem = new Item("Test Item", 20, initQuality);
+            GildedRose gildedRose = new GildedRose(new List<Item> { testItem });
+
+            for (var i = 0; i < daysPassed; i++) {
+                gildedRose.UpdateQuality();
+            }
+
+            Assert.That(testItem.Quality >= 0);
+        }
+
+        [Test]
+        public void Not_Increase_Aged_Brie_Quality_Over_Fifty()
+        {
+            var testItem = new Item("Aged Brie", 0, 30);
+            var gildedRose = new GildedRose(new List<Item> { testItem });
+            
+            gildedRose.UpdateQuality();
+
+            Assert.That(testItem.Quality <= 50);
+        }
+        
+        [Test]
+        public void Not_Sell_Or_Degrade_Sulfuras()
+        {
+            const int sellIn = 1337;
+            const int quality = 228;
+            
+            var testItem = new Item("Sulfuras, Hand of Ragnaros", sellIn, quality);
+            
+            var gildedRose = new GildedRose(new List<Item> { testItem });
+            gildedRose.UpdateQuality();
+            Assert.That(testItem.SellIn == sellIn);
+            Assert.That(testItem.Quality == quality);
         }
     }
 }
