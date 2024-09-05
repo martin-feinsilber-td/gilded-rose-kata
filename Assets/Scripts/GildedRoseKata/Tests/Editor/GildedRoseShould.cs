37d0e83e3b6b3d182d7d036a98a806407da2f37f
Mateusz Zawisza <mateusz.zawisza@tripledotstudios.com>
2024-09-05 16:28:04 +0200

    More tests

--- Assets/Scripts/GildedRoseKata/Tests/Editor/GildedRoseShould.cs
+++ Assets/Scripts/GildedRoseKata/Tests/Editor/GildedRoseShould.cs
@@ -7,7 +7,7 @@ using NUnit.Framework;
 namespace GildedRoseKata.Tests.Editor
 {
     [TestFixture]
-    public class GildedRoseShould 
+    public class GildedRoseShould
     {
         [Test]
         public void Rename_Me()
@@ -15,22 +15,64 @@ namespace GildedRoseKata.Tests.Editor
             Assert.Pass();
         }
 
-        [Test]
-        [TestCase()]
-        public void Item_Should_Have_Quality_Decreased_When_Time_Passes(int qualit)
+        [TestCase(5, 4)]
+        [TestCase(10, 9)]
+        public void Item_Should_Have_Quality_Decreased_When_Time_Passes(int quality, int expectedQuality)
         {
-            var item = new Item("", 5, 5);
+            var item = new Item("", 5, quality);
             var gildedRose = CreateGildedRoseWithAnItem(item);
-            
+
             gildedRose.UpdateQuality();
 
-            Assert.AreEqual(4, item.Quality);
+            Assert.AreEqual(expectedQuality, item.Quality);
         }
-        
-        [TestCase()]
+
+        [Test]
         public void Item_Never_Has_Quality_Less_Than_0()
         {
-            
+            var item = new Item("", 5, 0);
+            var gildedRose = CreateGildedRoseWithAnItem(item);
+
+            gildedRose.UpdateQuality();
+
+            Assert.AreEqual(0, item.Quality);
+        }
+
+        [TestCase(5, 4)]
+        [TestCase(10, 9)]
+        public void Item_Should_Have_SellIn_Decreased_When_Time_Passes(int sellIn, int expectedSellIn)
+        {
+            var item = new Item("", sellIn, 5);
+            var gildedRose = CreateGildedRoseWithAnItem(item);
+
+            gildedRose.UpdateQuality();
+
+            Assert.AreEqual(expectedSellIn, item.SellIn);
+        }
+
+        [TestCase(5, 3)]
+        [TestCase(10, 8)]
+        public void When_Item_SellIn_Is_Zero_Quality_Is_Degrading_Twice_As_Fast_When_Time_Passes(int quality, int expectedQuality)
+        {
+            var item = new Item("", 0, quality);
+            var gildedRose = CreateGildedRoseWithAnItem(item);
+
+            gildedRose.UpdateQuality();
+
+            Assert.AreEqual(expectedQuality, item.Quality);
+        }
+
+        // NOT TESTED NOT FINISHED! ! ! ! !
+        [TestCase(5, 3)]
+        [TestCase(10, 8)]
+        public void When_Item_Is_Aged_Brie_Quality_Increases_When_Time_Passes(int quality, int expectedQuality)
+        {
+            var item = new Item("Aged Brie", 5, quality);
+            var gildedRose = CreateGildedRoseWithAnItem(item);
+
+            gildedRose.UpdateQuality();
+
+            Assert.Greater(item.Quality, quality);
         }
 
         private GildedRose CreateGildedRoseWithAnItem(Item item)
